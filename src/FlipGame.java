be808d1290a061f3df3013bf31441835d545bf2a
SimStudent <738878650@qq.com>
2024-11-30 11:16:10 +0800

    Compele the first version of 3x3 table.

--- src/FlipGame.java
+++ src/FlipGame.java
@@ -68,24 +68,70 @@ public class FlipGame extends JFrame  {
         setVisible(true);
     }
 
-    private JLabel lights[][];
+    private Light lights[][];
+    private int clickCounnt = 0;
 
     public void createTable(){  // 创建游戏棋盘
         setLayout(null);  // 要设置为null，否则默认的布局 BorderLayout会让最后一个按钮异常
-        lights = new JLabel[3][3];
+        lights = new Light[3][3];
         for(int i = 0; i < 3; i++){
             for(int j = 0;j < 3;j++){
                 lights[i][j] = new Light(false);
                 // lights[i][j].setIcon(tools.getIconFromFile("REDx128.png"));
                 lights[i][j].setBounds(j*128, i*128, 128, 128);
                 add(lights[i][j]);
+
+                int row = i;
+                int col = j;
+                lights[i][j].addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        // TODO Auto-generated method stub
+                        // super.mouseClicked(e);
+                        System.out.println("Clicked");
+                        lights[row][col].changeState();
+                        if(row-1>=0 && row-1 <3)lights[row-1][col].changeState();
+                        if(row+1>=0 && row+1 <3)lights[row+1][col].changeState();
+                        if(col-1 >= 0 && col-1 <3)lights[row][col-1].changeState();
+                        if(col+1 >= 0 && col+1 <3)lights[row][col+1].changeState();
+                        clickCounnt++;
+                        if(Light.isAllBright(lights)){
+                            System.out.println("You Win! You click"+clickCounnt+"次！");
+                            
+                            // try{
+                            //     Thread.sleep(500);
+                            // }
+                            // catch (InterruptedException e1) {
+                            //     // TODO Auto-generated catch block
+                            //     e1.printStackTrace();
+                            // }
+                            // Light.resetBrights(lights);
+
+                            Timer timer = new Timer(500, new ActionListener() {
+                                @Override
+                                public void actionPerformed(ActionEvent e) {
+                                    Light.resetBrights(lights);
+                                    clickCounnt = 0;
+                                }
+                            });
+                            timer.setRepeats(false);
+                            timer.start();
+
+                        }
+                    }
+                });
+
+
             }
         }
         }
     
 
 
-
+    public static void winPane(){
+        // 创建一个JOptionPane对象
+        new JOptionPane("朋友你赢了!", JOptionPane.INFORMATION_MESSAGE).showOptionDialog(null, getFrames(), null, ALLBITS, ABORT, null, getWindows(), getOwnerlessWindows());
+    }
 
 
     public static void main(String[] args) throws Exception {
@@ -116,21 +162,65 @@ class Light extends JLabel{
         this.isBright = isBright;
         updateColor();
 
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                // TODO Auto-generated method stub
-                // super.mouseClicked(e);
-                System.out.println("Clicked");
-                changeState();
-            }
-        });
+        // addMouseListener(new MouseAdapter() {
+        //     @Override
+        //     public void mouseClicked(MouseEvent e) {
+        //         // TODO Auto-generated method stub
+        //         // super.mouseClicked(e);
+        //         System.out.println("Clicked");
+        //         changeState();
+        //     }
+        // });
 
     }
 
     public void changeState(){
         isBright = !isBright;
         updateColor();
+    }
+
+    public void resetState(){
+        isBright = false;
+        updateColor();
+    }
+
+
+    public static void resetBrights(Object lights){
+        if(lights instanceof Light){((Light)lights).resetState();}
+        else if(lights instanceof Light[]){
+            for(Light light: (Light[]) lights){
+                light.resetState();
+            }
+        }
+        else if(lights instanceof Light[][]){
+            for(Light[] row: (Light[][]) lights){
+                for(Light light: row){
+                    light.resetState();;
+                }
+            }
+
+        }
+        else throw new IllegalArgumentException("Lights must be an array of Light objects.");
+    }
+
+
+    public static Boolean isAllBright(Object lights){
+        if(lights instanceof Light){return ((Light)lights).isBright();}
+        else if(lights instanceof Light[]){
+            for(Light light: (Light[]) lights){
+                if(!light.isBright()) return false;
+            }
+            return true;
+        }
+        else if(lights instanceof Light[][]){
+            for(Light[] row: (Light[][]) lights){
+                for(Light light: row){
+                    if(!light.isBright()) return false;
+                }
+            }
+            return true;
+        }
+        else throw new IllegalArgumentException("Lights must be an array of Light objects.");
     }
 
     private void updateColor(){
